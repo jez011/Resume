a3072505a7e7632b9b19ccaf6ca57ecd647a3c58
Jordon Thill <148517075+jez011@users.noreply.github.com>
2024-07-28 13:22:28 -0400

    Resume.cpp
    Increase in efficiency using else if statements as opposed to if statements; also creates an else statement for incorrect input.

--- Resume/Resume.cpp
+++ Resume/Resume.cpp
@@ -1,43 +1,42 @@
-﻿#include <iostream>
-using namespace std; 
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
     std::string skills;
     std::string Programming = "Python, C++, Basic Scripting (CMD), linux terminal, some SQL, LaTeX, HTML, Some Basic Assembly Language.";
     std::string Network = "Experience with Wireshark, Packet sniffing, Network spoofing, AWS.";
-    std::string Software = "Linux OS (Mint, Kali), Word, Excel, Powerpoint, Wireshark, nmap, virtual machines, Visual Studio, Windows 7 ‑ 11 OS";
+    std::string Software = "Linux OS (Mint, Kali), Word, Excel, Powerpoint, Wireshark, nmap, virtual machines, Visual Studio, Windows 7 ? 11 OS";
     std::string Certifications = "Cybersecurity";
 
     std::cout << "Hello! What skills would you like to view on my resume?\n";
     std::cout << "The options are: Programming, Network, Software,\n" << "Certifications, Currently working on (CWO)\n";
-    
+
     short x = 0;
-    while (x<4) {
+    while (x < 4) {
         std::cin >> skills;
         if (skills == "Programming") {
             std::cout << Programming;
             std::cout << "\n";
         }
-        
-        if (skills == "Network") {
+        else if (skills == "Network") {
             std::cout << Network;
             std::cout << "\n";
         }
-        
-        if (skills == "Software") {
+        else if (skills == "Software") {
             std::cout << Software;
             std::cout << "\n";
         }
-        
-        if (skills == "Certifications") {
+        else if (skills == "Certifications") {
             std::cout << Certifications;
             std::cout << "\n";
         }
+        else
+            std::cout << "Incorrect input. Cap sensitive.\n";
         x++;
-        
     }
-        return 0;
     
-}
+    return 0; 
 
+}
